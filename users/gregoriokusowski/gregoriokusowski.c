f9e9e2c8b07c81b4457338351dbc97d47e9d3d27
Gregorio Kusowski <gregorio.kusowski@gmail.com>
2020-11-01 17:18:22 +0100

    Fix right pinky

--- users/gregoriokusowski/gregoriokusowski.c
+++ users/gregoriokusowski/gregoriokusowski.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,
       KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC},
   {LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G,
-      KC_H, KC_J, KC_K, KC_L, KC_SCLN, RCTL_T(KC_QUOT)},
+      KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT},
   /*{LCTL_T(KC_ESC), LSFT_T(KC_A), LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G,*/
       /*KC_H, RGUI_T(KC_J), RALT_T(KC_K), RCTL_T(KC_L), RSFT_T(KC_SCLN), RCTL_T(KC_QUOT)},*/
   {KC_LSFT, LT(NUMBERS, KC_Z), KC_X, KC_C, KC_V, KC_B,
